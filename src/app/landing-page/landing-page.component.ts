86e1251d9f71ba5a3cdaa2bc95ab009617ecd812
Ashutosh Sharma <51715335+Ashutosh0Sharma@users.noreply.github.com>
2024-10-30 00:40:23 +0530

    Added the button to navbar

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MatDrawerMode } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints  } from '@angular/cdk/layout';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,4 +10,25 @@ import { MatDrawerMode } from '@angular/material/sidenav';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+
+  isLargeScreen: boolean = false;
+  isSidenavOpen: boolean = false;
+  showCloseButton: boolean = false;
+  constructor(private observer: BreakpointObserver, private router: Router) { }
+
+  ngOnInit(): void {
+    // Observe changes to screen width and update `isLargeScreen` accordingly
+    this.observer.observe([Breakpoints.Small, Breakpoints.XSmall, Breakpoints.Medium])
+      .subscribe(result => {
+        this.isLargeScreen = !result.matches;
+        this.isSidenavOpen = this.isLargeScreen;
+        this.showCloseButton = !this.isLargeScreen && this.isSidenavOpen;
+      });
+  }
+
+
+  toggleSidenav() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+    this.showCloseButton = !this.isLargeScreen && this.isSidenavOpen;
+  }
 }
